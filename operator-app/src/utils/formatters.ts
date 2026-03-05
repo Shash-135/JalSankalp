5f6c9a112c82e00b38b9a5943dcf5f32e711f342
Shash-135 <shashanktalekar3@gmail.com>
2026-03-05 22:14:27 +0530

    "Reseting the operator app"

--- operator-app/src/utils/formatters.ts
+++
@@ -1,4 +0,0 @@
-export const formatTime = (value: string | number | Date) =>
-  new Date(value).toLocaleTimeString();
-export const formatDate = (value: string | number | Date) =>
-  new Date(value).toLocaleDateString();
